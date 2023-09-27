31eaf0aff7a5b7de407a43fac558c9a0f9b803bc
Said Nahúm <70162272+saidnahum@users.noreply.github.com>
2023-09-27 19:28:53 +0000

    feat: Add Home Page type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
-import Image from 'next/image'
+import { Metadata } from "next";
+import { SliceZone } from "@prismicio/react";
 
-export default function Home() {
-  return (
-    <main className="">
-      <h1 className='text-7xl font-display font-bold'>
-        Main App!!
-      </h1>
-    </main>
-  )
+import { createClient } from "@/prismicio";
+// import { components } from "@/slices";
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("home_page");
+
+  return <div>It Works!!</div>
+
+  // return <SliceZone slices={page.data.slices} components={components} />;
 }
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("home_page");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
+}
+
