67e48c073f131e8d6d22a56ddd2713fbfbf224a6
Said Nahúm <70162272+saidnahum@users.noreply.github.com>
2023-09-29 17:56:14 +0000

    feat: Styling Footer, Header and Logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import { createClient } from '@/prismicio';
 import { PrismicNextLink } from '@prismicio/next';
 import Link from 'next/link';
+import Logo from '@/components/Logo';
+import Bounded from '@/components/Bounded';
 
 const Footer = async () => {
 
@@ -8,23 +10,25 @@ const Footer = async () => {
   const settings = await client.getSingle("settings");
 
   return (
-    <footer>
-      <Link href="/">
-        {settings.data.site_title}
-      </Link>
-
-      <p>©{new Date().getFullYear()} {settings.data.site_title}</p>
-
-      <ul>
-        {settings.data.navigation.map(({ label, link }) => (
-          <li key={label}>
-            <PrismicNextLink field={link}>
-              {label}
-            </PrismicNextLink>
-          </li>
-        ))}
-      </ul>
-    </footer>
+    <Bounded as="footer">
+      <div className="flex sm:flex-row flex-col justify-between items-center gap-6">
+        <Link href="/">
+          <Logo/>
+        </Link>
+  
+        <p className="text-xs">©{new Date().getFullYear()} {settings.data.site_title}</p>
+  
+        <ul className="flex">
+          {settings.data.navigation.map(({ label, link }) => (
+            <li key={label}>
+              <PrismicNextLink field={link} className="p-3">
+                {label}
+              </PrismicNextLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Bounded>
   )
 }
 
