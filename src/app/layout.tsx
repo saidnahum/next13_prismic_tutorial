ead081c049a2d50e727805fc4b363c7a47bc8b6d
Said Nahúm <70162272+saidnahum@users.noreply.github.com>
2023-10-04 19:30:07 +0000

    feat: Add Prismic Preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,11 @@ import './globals.css';
 import type { Metadata, ResolvingMetadata } from 'next';
 import clsx from 'clsx';
 import { Nunito, Nunito_Sans } from 'next/font/google';
-import { createClient } from '@/prismicio';
+import { createClient, repositoryName } from '@/prismicio';
+import { PrismicPreview } from '@prismicio/next';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+
 
 const nunito = Nunito({
   subsets: ['latin'],
@@ -44,6 +46,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <div className="fixed bg-gradient-to-tr from-emerald-50 to-cyan-50 z-[-1] inset-0 opacity-60"/>
+        <PrismicPreview repositoryName={repositoryName}/>
       </body>
     </html>
   )
